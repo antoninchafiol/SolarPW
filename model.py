88abaf001bca90f6d301d76b4dc3b8113ea2e89c
antoninchafiol <antonin.chafiol@gmail.com>
2023-09-13 16:56:44 +0200

    [UPDT] - First check kwwith meaned values done

---
+++ model.py
@@ -0,0 +1,21 @@
+from sklearn.ensemble import RandomForestRegressor
+
+basic_randForest = RandomForestRegressor(
+    n_estimators=100, 
+    criterion='squared_error',
+    max_depth=None, 
+    min_samples_split=2, 
+    min_samples_leaf=1, 
+    min_weight_fraction_leaf=0.0, 
+    max_features=1.0, 
+    max_leaf_nodes=None, 
+    min_impurity_decrease=0.0, 
+    bootstrap=True,
+    oob_score=False, 
+    n_jobs=None, 
+    random_state=None, 
+    verbose=0, 
+    warm_start=False,
+    ccp_alpha=0.0,
+    max_samples=None
+) 
