85e129074decd8f5cd7eeae180147fe5caa890d7
antoninchafiol <antonin.chafiol@gmail.com>
2023-10-08 22:31:23 +0200

    [UPDT] - Updated models for tuning overnight

--- src/model.py
+++ src/model.py
@@ -3,20 +3,22 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+
+import numpy as np
 
 def model_testing(X,y, mtype):
     # Quick test of models, without any tunings.
     res = []
     x_train, x_dev, y_train, y_dev = train_test_split(X, y, test_size=0.2, random_state=42)
     if mtype=='SVM':
-        res= plain_testing(x_train, x_dev, y_train, y_dev, SVR())
+        res= model_testing_SVR(x_train, x_dev, y_train, y_dev)
     elif mtype=='DT':
-        res= plain_testing(x_train, x_dev, y_train, y_dev, DecisionTreeRegressor())
+        res= model_testing_DT(x_train, x_dev, y_train, y_dev)
     elif mtype=='RF':
-        res= plain_testing(x_train, x_dev, y_train, y_dev, RandomForestRegressor())
+        res= model_testing_RF(x_train, x_dev, y_train, y_dev)
     elif mtype=='GB':
-        res= plain_testing(x_train, x_dev, y_train, y_dev, GradientBoostingRegressor())
+        res= model_testing_GB(x_train, x_dev, y_train, y_dev)
     elif mtype=='NN':
         res= model_testing_NN(x_train, x_dev, y_train, y_dev)
     return res
@@ -33,16 +35,86 @@ def plain_testing(x_train, x_dev, y_train, y_dev, model):
 
 # For future hyperparameters tuning
 def model_testing_SVR(x_train, x_dev, y_train, y_dev):
-    return 0
+    model = SVR()
+    param_dist = {
+        'kernel': ['linear','rbf', 'poly'],
+        'C': np.arange(1,100, 10),
+        'gamma': np.arange(1,10,0.1),
+    }
+    tuning = RandomizedSearchCV(model, param_dist, n_iter=50 , n_jobs=-1, random_state=42)
+    tuning.fit(x_train, y_train)
+    print(tuning.best_params_)
+    preds = tuning.best_estimator_.predict(x_dev)
+    return tuning.best_estimator_, {
+        'r2': r2_score(preds, y_dev),
+        'MAE': mean_absolute_error(preds, y_dev), 
+        'MSE': mean_squared_error(preds, y_dev, squared=True),
+        'RMSE': mean_squared_error(preds, y_dev, squared=False)
+    }
 
 def model_testing_DT(x_train, x_dev, y_train, y_dev):
-    return 0
+    model = DecisionTreeRegressor()
+    param_dist = {
+        'criterion': ['mse', 'mae'], 
+        'splitter': ['best', 'random'],
+        'max_depth': np.arange(1,20), 
+        'min_samples_split': np.arange(2, 21), 
+        'max_samples_leaf': np.arange(1,20),
+    }
+    tuning = RandomizedSearchCV(model, param_dist, n_iter=50 , n_jobs=-1, random_state=42)
+    tuning.fit(x_train, y_train)
+    print(tuning.best_params_)
+    preds = tuning.best_estimator_.predict(x_dev)
+    return tuning.best_estimator_, {
+        'r2': r2_score(preds, y_dev),
+        'MAE': mean_absolute_error(preds, y_dev), 
+        'MSE': mean_squared_error(preds, y_dev, squared=True),
+        'RMSE': mean_squared_error(preds, y_dev, squared=False)
+    }
 
 def model_testing_RF(x_train, x_dev, y_train, y_dev):
-    return 0
+    model = RandomForestRegressor()
+    param_dist = {
+        'n_estimators': np.arange(10,200),
+        'criterion': ['mse', 'mae'],
+        'max_depth': np.arange(1,20), 
+        'min_samples_split': np.arange(2, 21), 
+        'max_samples_leaf': np.arange(1,20),
+        'max_features': ['auto', 'sqrt', 'log2', None],
+    }
+    tuning = RandomizedSearchCV(model, param_dist, n_iter=50 , n_jobs=-1, random_state=42)
+    tuning.fit(x_train, y_train)
+    print(tuning.best_params_)
+    preds = tuning.best_estimator_.predict(x_dev)
+    return tuning.best_estimator_, {
+        'r2': r2_score(preds, y_dev),
+        'MAE': mean_absolute_error(preds, y_dev), 
+        'MSE': mean_squared_error(preds, y_dev, squared=True),
+        'RMSE': mean_squared_error(preds, y_dev, squared=False)
+    }
 
 def model_testing_GB(x_train, x_dev, y_train, y_dev):
-    return 0
-
+    model = GradientBoostingRegressor()
+    param_dist = {
+        'n_estimators': np.arange(10,200),
+        'learning_rate': np.arange(0.01, 0.5, 0.05),
+        'max_depth': np.arange(1,10), 
+        'subsample': np.arange(0.1,1.0,0.1),
+        'min_samples_split': np.arange(2, 21), 
+        'max_samples_leaf': np.arange(1,20),
+    }
+    tuning = RandomizedSearchCV(model, param_dist, n_iter=50 , n_jobs=-1, random_state=42)
+    tuning.fit(x_train, y_train)
+    print(tuning.best_params_)
+    preds = tuning.best_estimator_.predict(x_dev)
+    return tuning.best_estimator_, {
+        'r2': r2_score(preds, y_dev),
+        'MAE': mean_absolute_error(preds, y_dev), 
+        'MSE': mean_squared_error(preds, y_dev, squared=True),
+        'RMSE': mean_squared_error(preds, y_dev, squared=False)
+    }
 def model_testing_NN(x_train, x_dev, y_train, y_dev):
     return 0
+
+
+
